6d57f4b88b99aa4b9445e09917d215cdf647b23b
27areeser <27areeser@students.bhasd.org>
2025-04-10 09:01:09 -0400

    second commit

--- MovieCollection.java
+++ MovieCollection.java
@@ -9,18 +9,49 @@ public class MovieCollection
         movies = new ArrayList<Movie>();
     }
 
-    // adds a new movie to the movie collection
+    
+    // adds a new movie to the movie collection and sort
     public void addMovie(String title, int year, double rating)
     {
         // Implement this method
+        Movie newMovie = new Movie(title, year, rating);
+        // Conditional for beginning of array
+        if ((movies.size() == 0) || (newMovie.getRating() >= movies.get(0).getRating()))
+        {
+            movies.add(0, newMovie);
+        }
+        else if (newMovie.getRating() < movies.get(movies.size() - 1).getRating())
+        {
+            movies.add(newMovie);
+        }
+        else
+        {
+            for (int i = 0; i < movies.size() - 1; i++)
+            {
+                if (newMovie.getRating() <= movies.get(i).getRating() && newMovie.getRating() > movies.get(i+1).getRating())
+                {
+                    movies.add(i, newMovie);
+                }
+            }
+        }
+        System.out.println("Movie added successfully!");
     }
+    
 
     // removes a movie from the movie collection
     // return true if movie was successfully removed
     // return false if movie cannot be found
     public boolean removeMovie(String title)
     {
-        // Implement this method
+        for(int i = 0; i < movies.size(); i++)
+        {
+            if (title.equals(movies.get(i)))
+            {
+                movies.remove(i);
+                return true;
+            }
+        }
+        return false;
     }
 
     // search for a movie
@@ -38,9 +69,11 @@ public class MovieCollection
         // Implement this method
     }
 
+    /*
     // sorts movies by rating - use Bubblesort
     private void sortMoviesByRating()
     {
         // Implement this method
     }
+    */
 }
--- MovieManager.java
+++ MovieManager.java
@@ -1,3 +1,4 @@
+import java.util.*;
 public class MovieManager
 {
     /* The main method to control program.
@@ -19,4 +20,52 @@ public class MovieManager
     Enter rating (out of 10): 8.8
     Movie added successfully!
     */ 
+   public static void main(String [] args)
+    {
+        Scanner scanner = new Scanner(System.in);
+        MovieCollection movieCollection = new MovieCollection();
+        int choice = scanner.nextInt();
+
+
+        while (choice != 5)
+        {
+            writeOptions();
+            choice = Integer.parseInt(scanner.nextLine());
+            if (choice == 1)
+            {
+                System.out.print("Movie title: ");
+                String title = scanner.nextLine();
+                System.out.print("Movie year: ");
+                int year = Integer.parseInt(scanner.nextLine());
+                System.out.print("Movie rating [0.0 - 10.0]: ");
+                double rating = Double.parseDouble(scanner.nextLine());
+                movieCollection.addMovie(title, year, rating);
+            }
+            /*
+            else if (choice == 2)
+            {
+                System.out.print("Movie title: ");
+                String title = scanner.nextLine();
+                System.out.print("Movie year: ");
+                int year = Integer.parseInt(scanner.nextLine());
+                System.out.print("Movie rating [0.0 - 10.0]: ");
+                double rating = Double.parseDouble(scanner.nextLine());
+                movieCollection.addMovie(title, year, rating);
+            }
+            */
+        }
+
+
+    }
+
+    public static void writeOptions()
+    {
+        System.out.println("Welcome to the Movie Manager!");
+        System.out.println("1. Add a Movie");
+        System.out.println("2. Remove a Movie");
+        System.out.println("3. Search for a Movie");
+        System.out.println("4. Display all Movies");
+        System.out.println("5. Exit");
+        System.out.print("Enter a number: ");
+    }    
 }
